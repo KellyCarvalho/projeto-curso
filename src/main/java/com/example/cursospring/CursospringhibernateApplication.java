d8f2b9d7ba8f1766955321f96373228c9a2a39a5
KellyCarvalho <18kellycarvalho@gmail.com>
2021-04-12 17:40:17 -0300

    Criando instanciação de objetos na compilação

--- src/main/java/com/example/cursospring/CursospringhibernateApplication.java
+++ src/main/java/com/example/cursospring/CursospringhibernateApplication.java
@@ -1,13 +1,31 @@
 package com.example.cursospring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.cursospring.domain.Categoria;
+import com.example.cursospring.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursospringhibernateApplication {
+public class CursospringhibernateApplication implements CommandLineRunner {
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringhibernateApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Sala");
+		Categoria cat3 = new Categoria(null,"Cozinha");
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		
 	}
 
 }
